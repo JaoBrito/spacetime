cbdf71395922425ebcf4f4967f19a0e7af7560df
JaoBrito <jvbrito02@gmail.com>
2023-05-19 16:10:24 -0300

    proximo de 50 min - aula 3

--- server/source/server.ts
+++ server/source/server.ts
@@ -1,13 +1,23 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
-  origin: 'true', // Todas as URLs de front-end poderão acessar nosso back-end
+  origin: true, // Todas as URLs de front-end poderão acessar nosso back-end
   // origin: ['http://localhost:3000', 'http://rocketseat.com.br/'], => Para especificar quem poderá acessar o back
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
