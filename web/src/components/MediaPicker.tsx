e126cc37a6b1ea19b1f69d645f4f256a28c5c6cd
JaoBrito <jvbrito02@gmail.com>
2023-05-26 17:22:20 -0300

    aula 05 - 35:58 (verificar rota)

--- web/src/components/MediaPicker.tsx
+++ web/src/components/MediaPicker.tsx
@@ -11,21 +11,25 @@ export function MediaPicker() {
     if (!files) {
       return
     }
+
     const previewURL = URL.createObjectURL(files[0])
+
     setPreview(previewURL)
   }
+
   return (
     <>
       <input
         onChange={onFileSelected}
         type="file"
-        id="midia"
+        name="coverUrl"
+        id="media"
         accept="image/*"
         className="invisible h-0 w-0"
       />
 
       {preview && (
-        // eslint-disable-next-line @next/next/no-img-element
+        // eslint-disable-next-line
         <img
           src={preview}
           alt=""
